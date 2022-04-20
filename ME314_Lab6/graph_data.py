20c1f418112c2d60a08e12bbbfcec0feeb60e48a
Dylan Ballback <44906769+dylanballback@users.noreply.github.com>
2022-04-19 23:54:23 -0400

    update

--- ME314_Lab6/graph_data.py
+++ ME314_Lab6/graph_data.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from Database import Database
 import matplotlib.pyplot as plt
 
@@ -6,18 +7,83 @@ database = Database("sensor_data.db")
 
 rows = database.all_sensor_data()
 
-time = [row[1] for row in rows]
+time = [row[1]/3600 for row in rows]
+
+
 temp = [row[2] for row in rows]
-humitiy = [row[3] for row in rows]
+humidity = [row[3] for row in rows]
 thermister = [row[4] for row in rows]
 photoresistor = [row[5] for row in rows]
 
+def temp_plot():
+    plt.plot(time, temp, label="Temperature")
+    plt.plot(time, thermister, label="Thermister")
+    plt.title("DHT-22 vs Thermister Temperature")
+    plt.xlabel("Time (Hour)")
+    plt.ylabel("Temperature (Deg C)")
+    plt.legend()
+    plt.show()
 
-plt.plot(time, temp, label= "Temperature")
-plt.plot(time, humitiy, label= "Humitiy")
-plt.plot(time, thermister, label= "Thermister")
-plt.plot(time, photoresistor, label= "Photoresistor")
-plt.legend()
-plt.show()
+def humity_plot():
+    plt.plot(time, humidity, label="Humidity")
+    plt.title("DHT-22 Humidity")
+    plt.xlabel("Time (Hour)")
+    plt.ylabel("Humidity Percentage")
+    plt.legend()
+    plt.show()
 
+def photo_plot():
+    plt.plot(time, photoresistor, label="Photoresistor")
+    plt.title("Photoresistor Sensor")
+    plt.xlabel("Time (Hour)")
+    plt.ylabel("Photoresistor Magintude")
+    plt.legend()
+    plt.show()
+
+
+def one_subplot():
+    plt.subplot(4,1,1)
+    plt.plot(time, temp)
+    plt.title("DHT-22 Temperature")
+    plt.xlabel("Time (Hour)")
+    plt.ylabel("Temperature (Deg C)")
+
+
+    plt.subplot(4,1,2)
+    plt.plot(time, humidity)
+    plt.title("DHT-22 Humidity")
+    plt.xlabel("Time (Hour)")
+    plt.ylabel("Humidity")
+
+
+    plt.subplot(4,1,3)
+    plt.plot(time, thermister)
+    plt.title("Thermister")
+    plt.xlabel("Time (Hour)")
+    plt.ylabel("Temperature (Deg C)")
+
+
+    plt.subplot(4,1,4)
+    plt.plot(time, photoresistor)
+    plt.title("Photoresistor")
+    plt.xlabel("Time (Hour)")
+    plt.ylabel("Photoresistor Magnitude")
+
+    plt.suptitle("Lab 5 Sensor Data Collection")
+
+    # set the spacing between subplots
+    plt.subplots_adjust(left=0.1,
+                        bottom=0.1, 
+                        right=0.9, 
+                        top=0.9, 
+                        wspace=0.4, 
+                        hspace=1.2)
+
+    plt.show()
+
+
+temp_plot()
+humity_plot()
+photo_plot()
+one_subplot()
 
